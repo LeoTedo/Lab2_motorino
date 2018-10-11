81635e04ced54701c2245859c6c3aa978d42a815
multimediale <multimediale@multimediale1>
2018-10-11 12:55:17 +0200

    Motorino funzionante

--- src/it/gov/itisfeltrinelli/motorino/Motorino.java
+++ src/it/gov/itisfeltrinelli/motorino/Motorino.java
@@ -1,25 +1,50 @@
 package it.gov.itisfeltrinelli.motorino;
 
 class Motorino {
-  String colore; 
-  float velocita;
-  String tipo;
-  boolean antifurto=false;
+  private String colore; 
+  private float velocita;
+  private String tipo;
+  private boolean antifurto=false;
+  private String targa;
+  private float maxVel;
   
-  public Motorino (String c, String t, float x) {
-	  colore=c;
-	  velocita=x;
-	  tipo=t;
-  }
-  public float  getVelocita(){
+  
+ 
+  public Motorino(String colore, float velocita, String tipo, String targa, float maxVel) {
+	this.colore = colore;
+	this.velocita = velocita;
+	this.tipo = tipo;
+	this.targa = targa;
+	this.maxVel = maxVel;
+}
+  
+public float  getVelocita(){
 	  return velocita;
   }
   public void accelera(float x){
 	  if(antifurto==false) {
 		  velocita+=x;
+		  if (velocita>maxVel) velocita=maxVel;  
 	  }
   }
-  void inserisciAntifurto(){
-	  antifurto=true;
+  void disinserisciAntifurto() {	  
+		  antifurto=false;
+  }
+  void inserisciAntifurto() {
+	  if (velocita==0) {
+		  antifurto=true;
+	  }	
+  }
+  public String getStatoantifurto() {
+	  if (antifurto==true) {
+		  return "Attivo";
+	  }
+	  else {
+		  return "Disattivato";
+	  }
+  }
+  public void frena(float x) {
+	  velocita-=x;
+	  if (velocita<0) velocita=0;
   }
 }
--- src/it/gov/itisfeltrinelli/motorino/TestMotorino.java
+++ src/it/gov/itisfeltrinelli/motorino/TestMotorino.java
@@ -3,7 +3,26 @@ package it.gov.itisfeltrinelli.motorino;
 public class TestMotorino {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Motorino m=new Motorino("rosso",0,"ciao","ef543kk",150);
+		m.inserisciAntifurto();
+		m.accelera(50);
+		System.out.println("Velocità del motorino: "+m.getVelocita());
+		m.disinserisciAntifurto();
+		m.accelera(70);
+		System.out.println("Velocità del motorino: "+m.getVelocita());
+        m.frena(45);
+        System.out.println("Velocità del motorino: "+m.getVelocita());
+        m.frena(45);
+        System.out.println("Velocità del motorino: "+m.getVelocita());
+        m.accelera(250);
+        System.out.println("Velocità del motorino: "+m.getVelocita());
+        m.inserisciAntifurto();
+        System.out.println("Velocità del motorino: "+m.getVelocita());
+        System.out.println("Stato antifurto: "+m.getStatoantifurto());
+        m.frena(200);
+        m.inserisciAntifurto();
+        System.out.println("Velocità del motorino: "+m.getVelocita());
+        System.out.println("Stato antifurto: "+m.getStatoantifurto());
 
 	}
 
